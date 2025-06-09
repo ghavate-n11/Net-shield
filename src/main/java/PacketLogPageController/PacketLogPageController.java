a4e51b0d53e9183f047065e02917639c7b58ea8f
Nilesh Ghavate <nileshghavate11@gmail.com>
2025-06-09 21:18:29 +0530

    Update PacketLogPageController.java

--- src/main/java/PacketLogPageController/PacketLogPageController.java
+++ src/main/java/PacketLogPageController/PacketLogPageController.java
@@ -4,7 +4,7 @@ import com.netshield.entity.PacketLog;
 import com.netshield.service.PacketLogService; // Import your service
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
+import org.springframework.ui.Model;// Import model
 import org.springframework.web.bind.annotation.GetMapping;
 
 import java.util.List;

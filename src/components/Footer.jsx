2de9141dedb4d81da7b8767de1812f1ca6e14509
Nilesh Ghavate <nileshghavate11@gmail.com>
2025-06-25 21:58:01 +0530

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,8 @@
 import React from 'react';
 import './Footer.css'; // Import the CSS file
 
-export default function Footer() {
+export default function Footer()//footer function 
+{
   return (
     <footer className="app-footer"> {/* Use the class name */}
       <p>&copy; 2025 Net Shield. All rights reserved.</p>
